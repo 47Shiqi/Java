34fe1ad441d6fe6ed3cfe3ce3b76f502dc48aa7b
47Shiqi <147263794+47Shiqi@users.noreply.github.com>
2025-09-22 08:34:06 -0400

    Update Student.java

--- ActivityStudentAcademia/Student.java
+++ ActivityStudentAcademia/Student.java
@@ -1,1 +1,30 @@
-a
+package javaPack;
+
+public class Student 
+{
+	private String firstname, lastname;
+	private int grade;
+	private int pGPA;
+	private int cGPA;
+	
+	public Student(String sFName, String sLName, int sGrade, int spGPA, int scGPA)
+	{
+		this.firstname = sFName;
+		this.lastname = sLName;
+		this.grade = sGrade;
+		this.pGPA = spGPA;
+		this.cGPA = scGPA;
+	}
+	
+	public String GetName()
+	{
+		String fullname = firstname + " " + lastname;
+		return fullname;
+	}
+	
+	public String GetGrade()
+	{
+		String fullname = firstname + " " + lastname;
+		return grade;
+	}
+}
