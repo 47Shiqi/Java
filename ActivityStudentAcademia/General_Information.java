5a8aedc220babb8897d181d694e8a355b356a9cd
47Shiqi <147263794+47Shiqi@users.noreply.github.com>
2025-09-25 08:01:36 -0400

    Update General_Information.java

--- ActivityStudentAcademia/General_Information.java
+++ ActivityStudentAcademia/General_Information.java
@@ -5,7 +5,9 @@ public class General_Information {
 	public static void main(String[] args) 
 	{
 		CollectInfo collect1 = new CollectInfo();
+		System.out.println("---------------------------------");
 		CollectInfo collect2 = new CollectInfo();
+		System.out.println("---------------------------------");
 		CollectInfo collect3 = new CollectInfo();
 	}
 }
