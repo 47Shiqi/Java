db31af86bdf7dc4f021f2aa865306fd30dfc5376
47Shiqi <147263794+47Shiqi@users.noreply.github.com>
2025-10-05 23:10:30 -0400

    Update CollectInfo.java

--- Airplane/CollectInfo.java
+++ Airplane/CollectInfo.java
@@ -1,21 +1,48 @@
 package javaPack;
 
-public class Get_Information 
+import java.util.Scanner;
+
+public class CollectInfo 
 {
-	String callsign = "AAA01";
-	double distance = 1;
+	String callsign;
+	double distance = 0.0;
 	int direction = 0;
 	int altitude = 0;
 	
-	public Get_Information()
+	Scanner allinput = new Scanner(System.in);
+	
+	public CollectInfo()
 	{
 		AllInfo();
 	}
 	
 	public void AllInfo()
 	{
-		Airplane planeinfo = new Airplane(callsign, distance, direction, altitude);
+		Airplane planeinfo1 = new Airplane();
+		Airplane planeinfo2 = new Airplane("AAA02", 15.8, 128, 30000);
 		
-		System.out.println(planeinfo.gainAlt());
+		System.out.print("Enter the call-sign of the third airplane: ");
+		callsign = allinput.nextLine();
+		
+		System.out.print("Enter its horizontal distance in miles from the tower (as a positive number): ");
+		distance = allinput.nextDouble();
+		
+		System.out.print("Enter its bearing from the tower (as an integer): ");
+		direction = allinput.nextInt();
+		
+		System.out.print("Enter its a positive altitude in feet (as an integer): ");
+		altitude = allinput.nextInt();
+		
+		Airplane planeinfo3 = new Airplane(callsign, distance, direction, altitude);
+		
+		planeinfo1.gainAlt(3);
+		planeinfo2.loseAlt(2);
+		planeinfo3.loseAlt(4);
+		planeinfo1.move(planeinfo2.distTo(planeinfo3), 65);
+		planeinfo2.move(8.0, 135);
+		planeinfo3.move(5.0, 55);
+		
+		Output ALLinfo = new Output();
+		ALLinfo.printEverything(planeinfo1, planeinfo2, planeinfo3);
 	}
 }
