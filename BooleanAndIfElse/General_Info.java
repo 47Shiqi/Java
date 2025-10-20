17bf416661ca0063b36fd8c6c121423b414fb0fb
47Shiqi <147263794+47Shiqi@users.noreply.github.com>
2025-10-19 22:21:03 -0400

    Add General_Info class to initialize CollectInfo

---
+++ BooleanAndIfElse/General_Info.java
@@ -0,0 +1,11 @@
+// main class to start the program by creating a CollectInfo object
+package javaPack;
+
+public class General_Info {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		CollectInfo collect = new CollectInfo();
+	}
+}
