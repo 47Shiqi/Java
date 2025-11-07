64dddc3272ea9bfb900245e7b11ccd069d0480df
47Shiqi <147263794+47Shiqi@users.noreply.github.com>
2025-11-06 22:30:49 -0500

    Update Calculate.java

--- WhileLoop/Calculate.java
+++ WhileLoop/Calculate.java
@@ -1,37 +1,51 @@
+// Class that calculates all info and return the info
+
 package javaPack;
 
 public class Calculate 
 {
-	private float aGrade;
-	private float totalGrade;
+	private double aGrade;
 	
-	public Calculate(float thegrade)
+	public Calculate(double thegrade)
 	{
+		// constructor that gets the grade info 
 		this.aGrade = thegrade;
 	}
 	
 	public double GetGPA()
 	{
-		double GPA;
-		GPA = aGrade/100 * 4.0;
-		return GPA;
+		// calculate the GPA
+		int GPA;
+		double decimalGPA;
+		GPA = (int) (aGrade * 4); // first multiply the grade by 4 and truncate the grade 
+		decimalGPA = GPA / 100.0; // then convert to a 4.0 scale GPA by diving the grade by 100 and keep two decimal places
+		return decimalGPA;
 	}
 	
 	public String GetLetter()
 	{
-		int MyGrade = (int) aGrade;
+		int MyGrade = (int) aGrade / 10; // get the first or first two digit(s) of a grade  
 		String letter;
-		switch (MyGrade) 
+		switch (MyGrade) // match the letter grade by its first or first two digit(s)
 		{
-	    case MyGrade <= 100 && MyGrade >=90:
+	    case 10:
 	    	letter = "A";
-	        break; // Optional: exits the switch block
-	    case MyGrade < 90 && MyGrade >=80:
+	        break; 
+	    case 9:
+	    	letter = "A";
+	        break;
+	    case 8:
 	    	letter = "B";
 	        break;
-	    
+	    case 7:
+	    	letter = "C";
+	        break;
+	    case 6:
+	    	letter = "D";
+	        break;
 	    default: 
-	    	letter = "Sorry, something went wrong for letter grade";
+	    	letter = "F"; // first digit is 5 or less
 		}
+		return letter;
 	}
 }
