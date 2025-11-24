50f477ea846e64ed49d0f8e4eaeb5d83f3505cc2
47Shiqi <147263794+47Shiqi@users.noreply.github.com>
2025-11-24 12:38:29 -0500

    Create MainProgram.java

---
+++ T_CharAtWithArrays/MainProgram.java
@@ -0,0 +1,8 @@
+package javaPack
+
+public class MainProgram //Parent class
+{
+  public static void main(String[] args)
+  {
+    ArrayChar arrchar = new ArrayChar();
+    System.out.println("Welcome to my letter counting words!");
