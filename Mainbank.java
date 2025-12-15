f3f19c5377d83bbf92cb939445a981db2a863ddb
47Shiqi <147263794+47Shiqi@users.noreply.github.com>
2025-12-15 08:01:12 -0500

    Update Mainbank.java

--- Mainbank.java
+++ Mainbank.java
@@ -1,7 +1,13 @@
 package javaPack;
 
-public class MainBank {
+import java.util.Scanner;
 
+public class MainBank {
+	static Scanner allinput = new Scanner(System.in);
+	
+	//private String accountHolder;
+	//private String balance;
+	
 	public static void main(String[] args)
 	{
 		//Object and constructor creation
@@ -12,6 +18,10 @@ public class MainBank {
 	{
 		//Set the mutators to set the data
 		BankAccount myAccount = new BankAccount();
+		
+		System.out.println("Enter the account holder's name: ");
+		String accountHolder = allinput.nextLine();
+		
 		myAccount.setAccountHolder("Mr. Pigis");
 		myAccount.setBalance(1000000);
 		
