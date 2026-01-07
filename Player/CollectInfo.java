bcef49b4ba4a31ff08d90450baf36b504f899f6e
47Shiqi <147263794+47Shiqi@users.noreply.github.com>
2026-01-07 02:05:32 -0500

    Create CollectInfo class for player management
    This class collects player information and manages player actions during the game.

---
+++ Player/CollectInfo.java
@@ -0,0 +1,116 @@
+// Class that collect all info 
+package javaPack;
+
+import java.util.Scanner;
+
+public class CollectInfo 
+{
+	// declare and initialize variables
+	String name;
+	int x;
+	int y;
+	int z;
+	int hp;
+	int dir;
+	
+	Scanner allinput = new Scanner(System.in);
+	
+	public CollectInfo()
+	{
+		AllInfo();
+	}
+	
+	public void AllInfo()
+	{
+		Player p1 = new Player();
+		System.out.println(p1.toString());
+		System.out.println("Default player P1 is created!");
+		System.out.println("------------------------------");
+		
+		System.out.println("Please provide the name, x, y, and z coordinates for the 2nd player");
+		System.out.print("name: ");
+		name = allinput.nextLine();
+		
+		System.out.print("x coordinate(enter an integer please): ");
+		x = allinput.nextInt();
+		
+		System.out.print("y coordinate(enter an integer please): ");
+		y= allinput.nextInt();
+		
+		System.out.print("z coordinate(enter an integer please): ");
+		z = allinput.nextInt();
+		allinput.nextLine();
+		
+		Player p2 = new Player(name,x,y,z);
+		
+		System.out.println(p2.toString());
+		System.out.println("Player " + name + " is created!");
+		System.out.println("------------------------------");
+		
+		System.out.println("Please provide the name, x, y, and z coordinates, health, and direction"
+				+ " for the 3rd player");
+		System.out.print("name: ");
+		name = allinput.nextLine();
+		
+		System.out.print("x coordinate(enter an integer please): ");
+		x = allinput.nextInt();
+		
+		System.out.print("y coordinate(enter an integer please): ");
+		y= allinput.nextInt();
+		
+		System.out.print("z coordinate(enter an integer please): ");
+		z = allinput.nextInt();
+		
+		System.out.print("health(enter an integer please): ");
+		hp = allinput.nextInt();
+		
+		System.out.print("direction(enter an integer from 1-6 please): ");
+		dir = allinput.nextInt();
+		
+		Player p3 = new Player(name,x,y,z,hp,dir);
+		
+		System.out.println(p3.toString());
+		System.out.println("Player " + name + " is created!");
+		System.out.println("------------------------------");
+		
+		
+		//play the game
+		System.out.println("The game is in progress...");
+		p3.setHP(30);
+		System.out.println(p3.getName() + "'s hp is set to " + p3.getHP());
+		
+		p1.setDirection(2);
+		p2.setDirection(-2);
+		System.out.println(p1.getName() + "'s direction is set to " + p1.getDirection());
+		System.out.println(p2.getName() + "'s direction is set to " + p2.getDirection());
+		
+		p1.move(4, 2);
+		p2.move(1, 33);
+		System.out.println(p1.getName() + " moves 2 units to the west");
+		System.out.println(p2.getName() + " moves 33 units to the north");
+		
+		p3.teleport(p2);
+		System.out.println(p3.getName() + " is teleported to " + p2.getName() + " location.");
+		
+		System.out.println("distance between " + p3.getName() + " and the origin is " + p3.getDistance(0,0,0));
+		System.out.println("distance between " + p1.getName() + " and " + p2.getName() + " is " + p1.getDistance(p2));
+		
+		p2.attack(p1,4);
+		System.out.println(p2.getName() + " attempts to attack " + p1.getName());
+		System.out.println(p2.getName() + " - hp: " + p2.getHP() + ", " + p1.getName() + " - hp: " + p1.getHP());
+		p1.attack(p3,55);
+		System.out.println(p1.getName() + " attempts to attack " + p3.getName());
+		System.out.println(p1.getName() + " - hp: " + p1.getHP() + ", " + p3.getName() + " - hp: " + p3.getHP());
+		
+		
+		System.out.println("------------------------------");
+
+		//print the final state of all the players
+		System.out.println(p1.toString());
+		System.out.println("------------------------------");
+		System.out.println(p2.toString());
+		System.out.println("------------------------------");
+		System.out.println(p3.toString());
+		System.out.println("------------------------------");
+	}
+}
