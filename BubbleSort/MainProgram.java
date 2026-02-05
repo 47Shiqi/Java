17138a2b73cae7bdb511aeadaabdc03ce0fb9476
47Shiqi <147263794+47Shiqi@users.noreply.github.com>
2026-02-05 07:44:10 -0500

    Update MainProgram.java

--- BubbleSort/MainProgram.java
+++ BubbleSort/MainProgram.java
@@ -1,11 +1,11 @@
 package javaPack;
 
-public class MainProgram {
-
+public class MainProgram //main class that runs the program
+{
 	public static void main(String[] args) 
 	{
 		BubbleSort array = new BubbleSort();
-		int[] myArray = array.Sort();
+		int[] myArray = array.Sort(); //use the method Sort from the class BubbleSort
 			
 		//Output Result
 		for(int n=0; n<myArray.length; n++)
